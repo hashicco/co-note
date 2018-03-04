d2d6d6af2d552c5ed1b98b276640089579fd492e
hashicco <hashicco.dev@gmail.com>
2018-03-05 00:02:26 +0900

    feat: oauthは一旦なしで

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228101435) do
-
-  create_table "authentications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
-    t.integer "user_id", null: false
-    t.string "provider", null: false
-    t.string "uid", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
-  end
+ActiveRecord::Schema.define(version: 20180228101434) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer "group_id", null: false
